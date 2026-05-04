264fb5a5ab4cf4f8d93f27f2db253e1b9826cd4e
bagasadit123 <bagasadit430@gmail.com>
2026-05-04 19:24:28 +0700

    mendefinisikan prosedur diluar class

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -25,5 +25,17 @@ public:
     Mahasiswa(string pnama) :nama(pnama) {
         setID();
     };
-
 };
+
+int Mahasiswa::nim = 0;
+
+//pendefinisial prosedur - prosedur diluar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
