f040c1ce5c40cb6e770c4ff6bcd3c91bb6746a8f
bagasadit123 <bagasadit430@gmail.com>
2026-05-04 19:26:11 +0700

    deklarasi variabel

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -14,12 +14,3 @@ public:
         return this->judul;
     }
 };
-
-int main() 
-{
-    buku bukunya;
-    // bukunya.setJudul("Matematika");
-    // cout << bukunya.getJudul();
-    cout << bukunya.setJudul("Matematika").getJudul();    //chain function calls
-    return 0;
-}
